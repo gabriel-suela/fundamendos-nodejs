59d115da93c313485df62223a41e8faa7043afb1
gabriel-suela <gscsuela@gmail.com>
2022-09-29 13:16:43 -0300

    route query e body

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ const express = require ('express');
 
 const app = express();
 
+app.use(express.json());
+
 app.get("/courses", (request, response) => {
     const query = request.query
     console.log(query);
@@ -10,6 +12,8 @@ app.get("/courses", (request, response) => {
 });
 
 app.post ("/courses", (request, response) => {
+    const body = request.body
+    console.log(body);
     return response.json(["Curso 1", "Curso 2", "Curso 3", "Curso 4"]);
 });
 
